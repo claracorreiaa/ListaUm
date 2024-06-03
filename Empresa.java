9e0885a4d96c586802fc839f7fd61d846d28a37d
claracorreiaa <168542381+claracorreiaa@users.noreply.github.com>
2024-06-03 08:34:21 -0300

    Create Empresa.java

---
+++ Empresa.java
@@ -0,0 +1,23 @@
+public class Empresa {
+	private String nome;
+	private String cnpj;
+	
+	public Empresa(String novoNome, String novoCnpj) {
+		this.nome = novoNome;
+		this.cnpj = novoCnpj;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	public void setCpnc(String novoCnpj) {
+		this.cnpj = novoCnpj;
+	}
+}
