6c4615c1db77940f096a175991c41093f24a27d5
claracorreiaa <168542381+claracorreiaa@users.noreply.github.com>
2024-06-03 08:39:25 -0300

    Create Professor.java

---
+++ Professor.java
@@ -0,0 +1,34 @@
+public class Professor {
+	
+	public class Professor {
+		private String nome;
+		private String departamento;
+		private String titulacao;
+		
+		public Professor(String novoNome, String novoDepartamento, String novaTitulacao) {
+			this.nome = novoNome;
+			this.departamento = novoDepartamento;
+			this.titulacao = novaTitulacao;
+		}
+		
+		public String getNome() {
+			return nome;
+		}
+		public String getDepartamento() {
+			return departamento;
+		}
+		public String getTitulacao() {
+			return titulacao;
+		}
+		
+		public void setNome(String novoNome) {
+			this.nome = novoNome;
+		}
+		public void setDepartamento(String novoDepartamento) {
+			this.departamento = novoDepartamento;
+		}
+		public void setTitulacao(String novaTitulacao) {
+			this.titulacao = novaTitulacao;
+		}
+}
+}
